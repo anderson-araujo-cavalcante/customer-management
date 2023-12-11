12e8ec8f80aee573d4be65258352b1ec3ed0a532
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-11 12:27:52 -0300

    implemented repositories

---
+++ src/CustomerManagement.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using CustomerManagement.Data.Context;
+using CustomerManagement.Domain.Entities;
+using CustomerManagement.Domain.Interfaces.Repositories;
+
+namespace CustomerManagement.Data.Repositories
+{
+    public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(CustomerManagementContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ src/CustomerManagement.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,87 @@
+﻿using CustomerManagement.Data.Context;
+using CustomerManagement.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CustomerManagement.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected CustomerManagementContext _context;
+        protected DbSet<TEntity> dbSet;
+
+        #region Ctor
+        public RepositoryBase(CustomerManagementContext dbContext)
+        {
+            _context = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            dbSet = _context.Set<TEntity>();
+        }
+        #endregion        
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public virtual async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await dbSet.AsNoTracking().CountAsync(predicate);
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public virtual async Task AddAsync(TEntity entity)
+        {
+            await dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task RemoveAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        #region IDisposable Members
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context?.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable Members
+    }
+}

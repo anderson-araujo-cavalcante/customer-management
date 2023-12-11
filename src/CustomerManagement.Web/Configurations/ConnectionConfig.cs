be02e81e7f61e7b1f2861a9c5b85cff470fe67a8
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-11 16:08:32 -0300

    add connection string

---
+++ src/CustomerManagement.Web/Configurations/ConnectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using CustomerManagement.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerManagement.Web.Configurations
+{
+    public static class ConnectionConfig
+    {
+        public static WebApplicationBuilder AddDbContext(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("WebApiDatabase");
+            builder.Services.AddDbContext<CustomerManagementContext>(
+            options => options.UseSqlServer(connectionString));
+
+            return builder;
+        }
+    }
+}

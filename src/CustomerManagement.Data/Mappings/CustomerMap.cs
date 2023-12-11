ad53662a4b237b3f2020d77b4a25e5ddac6ec268
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-11 11:19:12 -0300

    create customer entity configuration map

---
+++ src/CustomerManagement.Data/Mappings/CustomerMap.cs
@@ -0,0 +1,29 @@
+﻿using CustomerManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CustomerManagement.Data.Mappings
+{
+    public class CustomerMap : IEntityTypeConfiguration<Customer>
+    {
+        private const string TABLE_NAME = "Customers";
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable(TABLE_NAME);
+
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(_ => _.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(_ => _.Cep)
+                .IsRequired()
+                .HasMaxLength(10);
+        }
+    }
+}

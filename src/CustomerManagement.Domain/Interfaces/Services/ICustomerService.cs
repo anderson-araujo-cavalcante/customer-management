fb9517df423039a9fe32b38c89be5331061b7180
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-11 14:25:20 -0300

    create customer service

---
+++ src/CustomerManagement.Domain/Interfaces/Services/ICustomerService.cs
@@ -0,0 +1,9 @@
+﻿using CustomerManagement.Domain.Entities;
+using CustomerManagement.Domain.Interfaces.Repositories;
+
+namespace CustomerManagement.Domain.Interfaces.Services
+{
+    public interface ICustomerService : IRepositoryBase<Customer>
+    {
+    }
+}
---
+++ src/CustomerManagement.Domain/Services/CustomerService.cs
@@ -0,0 +1,46 @@
+﻿using CustomerManagement.Domain.Entities;
+using CustomerManagement.Domain.Interfaces.Repositories;
+using CustomerManagement.Domain.Interfaces.Services;
+using System.Linq.Expressions;
+
+namespace CustomerManagement.Domain.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+        }
+
+        public async Task AddAsync(Customer obj)
+        {
+            await _customerRepository.AddAsync(obj);
+        }       
+
+        public async Task<IEnumerable<Customer>> GetAllAsync(Expression<Func<Customer, bool>> predicate)
+        {
+            return await _customerRepository.GetAllAsync(predicate);
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            return await _customerRepository.GetByIdAsync(id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            await _customerRepository.RemoveAsync(id);
+        }
+
+        public async Task UpdateAsync(Customer entity)
+        {
+            await _customerRepository.UpdateAsync(entity);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
